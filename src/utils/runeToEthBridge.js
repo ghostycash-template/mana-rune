fe036ec0e1a52fb8e0307e9d98206f9bbacd4b42
ghosty-template <87552256+ghostycash-template@users.noreply.github.com>
2025-03-02 00:13:17 +0800

    Update runeToEthBridge.js

--- src/utils/runeToEthBridge.js
+++ src/utils/runeToEthBridge.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import RuneToEth from "./RuneToEth.json";
 // Replace with your deployed contract's address
-const contractAddress = "0x2e17Eb489a48D8cC2a15e928F53560528Bee9957";
+const contractAddress = "0xdA96f3D08c790D22761bd31613CfC8b019B52F36";
 
 const runeToEthBridge = new web3.eth.Contract(RuneToEth, contractAddress);
 
